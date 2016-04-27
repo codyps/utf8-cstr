331004f51aea9d8ffb7a5bb71a28f3a321f9204b
Cody P Schafer <dev@codyps.com>
2016-04-27 13:07:29 -0400

    deref, fmt display, fmt debug, and fix an off-by-one in embedded null checks

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
+#[cfg(test)]
+#[macro_use]
+extern crate assert_matches;
+
 use std::ffi::CStr;
 use std::str::Utf8Error;
 use std::convert::AsRef;
 use std::mem::transmute;
 use std::os::raw::c_char;
+use std::ops::Deref;
+use std::fmt;
+
 
 #[derive(Debug)]
 pub enum Utf8CStrError {
@@ -22,6 +29,20 @@ pub struct Utf8CStr {
     inner: CStr,
 }
 
+impl fmt::Debug for Utf8CStr {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        let s : &str = self.as_ref();
+        write!(fmt, "{:?}", s)
+    }
+}
+
+impl fmt::Display for Utf8CStr {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        let s : &str = self.as_ref();
+        write!(fmt, "{}", s)
+    }
+}
+
 impl Utf8CStr {
     /// Failable convertion from a CStr.
     ///
@@ -37,7 +58,7 @@ impl Utf8CStr {
     /// once.
     pub fn from_bytes(b: &[u8]) -> Result<&Utf8CStr, Utf8CStrError> {
         // FIXME: use from_bytes_with_nul when stablized
-        for (l, &v) in b[0..b.len() - 2].iter().enumerate() {
+        for (l, &v) in b[0..b.len() - 1].iter().enumerate() {
             if v == b'\0' {
                 return Err(Utf8CStrError::EmbeddedNulTerm(l));
             }
@@ -63,7 +84,7 @@ impl Utf8CStr {
 
 impl AsRef<str> for Utf8CStr {
     fn as_ref(&self) -> &str {
-        unsafe { transmute(self) }
+        unsafe { transmute(self.inner.to_bytes()) }
     }
 }
 
@@ -73,14 +94,22 @@ impl AsRef<CStr> for Utf8CStr {
     }
 }
 
+impl Deref for Utf8CStr {
+    type Target = str;
+    fn deref(&self) -> &Self::Target {
+        self.as_ref()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::Utf8CStr;
+    use super::Utf8CStrError;
     #[test]
     fn it_works() {
         Utf8CStr::from_bytes(b"hello\0").unwrap();
-        Utf8CStr::from_bytes(b"hell").err().unwrap();
-        Utf8CStr::from_bytes(b"hell\0d").err().unwrap();
-        Utf8CStr::from_bytes(&[8,1,23,4]).err().unwrap();
+        assert_matches!(Utf8CStr::from_bytes(b"hell").err().unwrap(), Utf8CStrError::NoNulTerm);
+        assert_matches!(Utf8CStr::from_bytes(b"hell\0d").err().unwrap(), Utf8CStrError::EmbeddedNulTerm(4));
+        assert_matches!(Utf8CStr::from_bytes(&[8,1,23,4,0xff, 0]).err().unwrap(), Utf8CStrError::Utf8Error(_));
     }
 }
